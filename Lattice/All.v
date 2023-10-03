fd24296cf8c6439fd1f8960e267c403a01904a6b
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2023-10-03 23:53:11 +0900

    rm All.v

--- Lattice/All.v
+++
@@ -1,8 +0,0 @@
-Require Export AFT.Lattice.Lattice.
-Require Export AFT.Lattice.Complat.
-Require Export AFT.Lattice.Complat.
-Require Export AFT.Lattice.Bilattice.
-Require Export AFT.Lattice.ProdBilattice.
-Require Export AFT.Lattice.Interval.
-
-
